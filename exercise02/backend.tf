96b380b5edfb976d438337520b5af7d2cb539e3c
oskiegarcia <ogarc74@gmail.com>
2019-11-07 23:49:26 +0800

    deleted old files

--- exercise02/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "oskiegarcia-mybucket"
-    key    = "mytf/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
--- exercise03/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "aws_region" {
-  description = "AWS region to launch servers."
-  default     = "ap-southeast-1"
-}
